577f4603c1352da5ff85d671d722979c6192c012
benn-dev <66074802+Dev-Beom@users.noreply.github.com>
2022-05-06 00:31:44 +0900

    Enable Batch Processin, Scheduling

--- spring-batch/src/main/kotlin/com/example/springbatch/SpringBatchApplication.kt
+++ spring-batch/src/main/kotlin/com/example/springbatch/SpringBatchApplication.kt
@@ -1,9 +1,13 @@
 package com.example.springbatch
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableBatchProcessing
+@EnableScheduling
 class SpringBatchApplication
 
 fun main(args: Array<String>) {
